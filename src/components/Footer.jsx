36a4c69c373855593bb51d16aa62febb9a9f8811
1juanherrera <juansebas751@gmail.com>
2023-08-17 21:45:18 -0500

    footer built

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,32 @@
-
-
+import logo from '../assets/xmovie.png';
+import bg from '../assets/footer-bg.jpg';
+import { Link } from 'react-router-dom';
 
 export const Footer = () => {
     return (
-        <div className="footer">
-            Footer
+        <div className="footer" style={{ backgroundImage: `url(${bg})` }}>
+            <div className="footer__content">
+                <div className="footer__content__logo container">
+                    <div className="footer__content__logo--logo">
+                        <img src={ logo } alt='logo' />
+                        <Link to='/'>xMovies</Link>
+                    </div>
+                </div>
+                <div className="footer__content__menus">
+                <div className="footer__content__menus--menu">
+                        <Link to="/">Home</Link>
+                        <Link to="/">Contact us</Link>
+                        <Link to="/">Term of services</Link>
+                        <Link to="/">About us</Link>
+                    </div>
+                    <div className="footer__content__menus--menu">
+                        <Link to="/">Live</Link>
+                        <Link to="/">FAQ</Link>
+                        <Link to="/">Premium</Link>
+                        <Link to="/">Privacy policy</Link>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
